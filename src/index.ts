efcf07ba4606bbeb06ed99f304101d4404d1faf8
yevhenii-sulim <bees127@ukr.net>
2024-03-23 21:39:58 +0200

    crezy pagination

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import Pagination from './ts/pagination'
+const key: string = "ef54c316f166b2a5913791e8b3f63a4a"
+const page: number = 1;
+fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&sort_by=popularity.desc`, {
+  headers: {
+    "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZjU0YzMxNmYxNjZiMmE1OTEzNzkxZThiM2Y2M2E0YSIsInN1YiI6IjY0NzBkZmZhYzVhZGE1MDBjMWEzNzhmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fljmrABHLVGUf2e0aWKvdHeTeR0ruZNkP26DhsQLuYM",
+    accept: 'application/json'
+  }
+}).then(data => data.json()).then(data => {
+  console.log(data)
+  return showPage(data)
+})
+
+
+const list: HTMLElement = document.querySelector('.pagination-container')
+function showPage(data:any) {
+  const pagination = new Pagination(data.total_pages, data.page)
+  list.append(...pagination.element.children)
+};
---
+++ src/ts/pagination.ts
@@ -0,0 +1,120 @@
+// interface Pag {
+//   totalPages: number,
+//   page: number,
+//   showedPage: number, 
+//   listPage:number[],
+// }
+
+// export default class Pagination {
+//   listPage:number[];
+//   element:HTMLUListElement
+//   constructor({totalPages: number,page: number,showedPage: number = 5}:Pag) {
+//     this.totalPages = totalPages;
+//     this.page = page;
+//     this.showedPage = showedPage;
+//     this.render(this.page)
+//     this.addEventListeners()
+//   }
+
+export default class Pagination {
+
+  listPage:number[];
+  element:HTMLUListElement
+
+  constructor(public totalPages: number,public page: number,public showedPage: number = 5) {
+    this.totalPages = totalPages;
+    this.page = page;
+    this.showedPage = showedPage;
+    this.render(this.page)
+    this.addEventListeners()
+  }
+  
+  getTeamplateList(page: number) {
+    const active: string = page === this.page ? 'active' : ''
+    return `<li class="pagination page ${active}" data-page-index="${page}"><button>${page}</button></li>`
+  }
+  
+  showedCountPages() {
+    const counterPages:number =
+      this.totalPages < this.showedPage
+        ? this.totalPages
+        : this.showedPage;
+    return counterPages;
+  }
+  
+  pages(pages:number) {
+    const wrapper: HTMLUListElement = document.querySelector('.pagination-list')
+    this.element = wrapper
+    const list: string[] = [];
+    const listPage: number[] = [];
+    for (let page = pages; page < this.showedCountPages() + pages; page++) {     
+      listPage.push(page)
+      list.push(this.getTeamplateList(page))
+    }
+    this.listPage= listPage
+    return list.join('')
+  }
+  
+  render(page: number) {
+    const listPage = this.pages(page)
+    this.element.insertAdjacentHTML('beforeend', listPage)
+  }
+  
+  upDateRender(page: number){
+    if(page > this.totalPages || page < 1) return
+    this.element.innerHTML = this.pages(page)
+  }
+
+  addEventListeners() {
+    const next:HTMLElement = this.element.parentNode.querySelector('.next') 
+    const prev:HTMLElement = this.element.parentNode.querySelector('.prev') 
+    
+    next.addEventListener('click', () => {
+      this.nextPage()
+    })
+    prev.addEventListener('click', () => {
+      this.prevPage()
+    })
+
+    this.element.addEventListener('click', (evt) => {
+      const carrentPage: HTMLElement = evt.target.closest('.page')
+      if (!carrentPage) {
+        return
+      };
+      const pageIndex: number = parseInt(carrentPage.dataset.pageIndex)
+      
+      this.setPage(pageIndex)
+    })
+  }
+  
+  setPage(pageIndex: number) {
+    if (pageIndex === this.page) return
+    if(pageIndex > this.totalPages || pageIndex < 1) return
+    const activePage:HTMLElement = document.querySelector('.page.active') 
+    
+    if (activePage) {
+      activePage.classList.remove('active')
+    }
+    
+    const carrentPage: HTMLElement = document.querySelector(`[data-page-index="${pageIndex}"]`)
+    carrentPage.classList.add('active')
+    
+    this.page = pageIndex
+  }
+
+  nextPage() {
+    const nextPage: number = this.page + 1
+    if (nextPage > Math.max(...this.listPage)) {
+      this.upDateRender(nextPage-(this.showedPage-1))
+    }
+    this.setPage(nextPage)
+  }
+
+  prevPage() {
+    const prevPage: number = this.page - 1
+    if ( prevPage < Math.min(...this.listPage)) {
+      this.upDateRender(prevPage)
+    }
+    this.setPage(prevPage)
+  }
+}
